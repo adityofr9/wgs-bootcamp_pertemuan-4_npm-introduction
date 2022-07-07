7c72a83de16fcc6b4692f353c4673ffcabe76569
adityofr9 <adityofr9@gmail.com>
2022-07-07 10:22:15 +0700

    Menginstall dan mencoba fungsi modul validator

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Mencoba npm Validator
 const validator = require('validator');
 
-console.log(validator.isEmail('adityofr9@gmail.com'));
+console.log(validator.isEmail('adityofr9@gmail.com')); //Hasil validasi bernilai true
+console.log(validator.isMobilePhone('08123456789', 'id-ID')); //Hasil akan bernilai true apabila format sesuai nomor telp Indonesia
